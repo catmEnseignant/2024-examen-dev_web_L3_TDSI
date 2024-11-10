eaa2f791e6acd3775ca62a27fad028225a1ec68e
ndoondol <gningngouye2001@gmail.com>
2024-11-10 00:13:59 +0000

    Update master.php

--- examen_dev_web/ressourses/layouts/master.php
+++ examen_dev_web/ressourses/layouts/master.php
@@ -6,6 +6,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="../../public/css/css1.css">
         <title>Gestion Patient</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     </head>
     <body>
     <?php include('navtop.php'); ?>
