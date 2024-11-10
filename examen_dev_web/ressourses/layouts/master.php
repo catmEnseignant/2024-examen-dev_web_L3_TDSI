45122accec65c4dac7c2652b20b11190f3cd3c7d
ndoondol <gningngouye2001@gmail.com>
2024-11-10 00:14:50 +0000

    master.php

--- examen_dev_web/ressourses/layouts/master.php
+++ examen_dev_web/ressourses/layouts/master.php
@@ -7,7 +7,8 @@
         <link rel="stylesheet" href="../../public/css/css1.css">
         <title>Gestion Patient</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    </head>
+    </head> 
+    
     <body>
     <?php include('navtop.php'); ?>
     <?php include('header.php'); ?>
