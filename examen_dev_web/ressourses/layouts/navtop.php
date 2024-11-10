f02008916cc1803e60ac7c4d84078b54af666b86
ndoondol <gningngouye2001@gmail.com>
2024-11-10 00:15:55 +0000

    navtop.php

--- examen_dev_web/ressourses/layouts/navtop.php
+++ examen_dev_web/ressourses/layouts/navtop.php
@@ -1,9 +1,21 @@
 <header class="pb-2 headerClass">
     <nav class="px-3 navbar navbar-expand-lg navbar-dark bg-dark fixed">
         <div class="container row px-1 containerClass">
-            <div class="col">
-                <a class="navbar-brand" href="#"><img class="imgClass1" src="../../public/images/Soloquizz.png"></a>
-            </div>
+            
+
+        <div class="col">
+    <a class="navbar-brand" href="#">
+        <img class="imgClass1 logo-circle" src="../../public/images/Soloquizz.png" alt="OLOQUIZZ">
+        
+    </a>
+
+
+    
+
+
+</div>
+
+
             <div class="col divClass1">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -12,18 +24,18 @@
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown">
                             <button class="px-3 btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img class="imgClass2" src="../../public/images/user.png">
+                                <i class="fas fa-user-circle" style="font-size: 24px; color: white;"></i> <!-- Icône de personne encercle -->
                             </button>
                             <ul class="dropdown-menu dropdown-menu-info">
                                 <li>
                                     <a class="dropdown-item text-dark" href="#">
-                                        <span<ion-icon name="person"></ion-icon></span>
                                         <strong id="auteur">&nbsp;Alassane Laye Diop</strong>
                                     </a>
                                 </li>
                                 <li>
                                     <a class="dropdown-item text-dark" href="#">
-                                        <span<ion-icon name="power"></ion-icon></span>&nbsp;Deconnexion</a>
+                                        <span><i class="fas fa-power-off"></i></span>&nbsp;Deconnexion
+                                    </a>
                                 </li>
                             </ul>
                         </li>
